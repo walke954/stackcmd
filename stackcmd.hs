0a23c5b9f2d70d3719e92e0bb48facb4f185800e
Jonathan Walker <walkjo53@gmail.com>
2021-09-04 18:39:56 -0700

    added show command

--- stackcmd.hs
+++ stackcmd.hs
@@ -13,13 +13,18 @@ mainLoop sk = do
     if (action == "exit")
         then return []
     else do
-        let (newsk, itms) = if (action == "add")
-            then (addToStack sk args, [])
-            else popStack sk iarg
+        let (newsk, itms) = evalCmd action args iarg sk
         if (length itms > 0)
             then putStrLn $ unwords itms
             else return ()
         mainLoop newsk
+
+evalCmd :: String -> [String] -> Int -> Stack -> (Stack, [String])
+evalCmd a args iarg sk
+    | a == "add" = (addToStack sk args, [])
+    | a == "take" = popStack sk iarg
+    | a == "show" = (sk, showStack sk)
+    | otherwise = (sk, [])
 
 prompt :: String -> IO String
 prompt str = do
@@ -35,15 +40,18 @@ getUserInput = do
         f = if (length str > 0) then head ws else ""
         xs = if (length str > 0) then tail ws else []
         iarg = getIntArg xs
-    if (shouldExit f xs || f == "add")
+    if (shouldExit f xs || shouldShow f xs || f == "add")
         then return (f, xs, 0)
     else if (f == "take" && iarg > -1)
         then return (f, [], iarg)
     else do
         if (f == "help")
-            then putStrLn "[commands]      [description]                       [example]\n- add [item]    add following items to stack        add 1 2 3 4\n- exit          exit program\n- help          list commands\n- take n        take n items off stack and print    take 4"
+            then putStrLn "[commands]      [description]                       [example]\n- add [item]    add following items to stack        add 1 2 3 4\n- exit          exit program\n- help          list commands\n- show          show items in current stack\n- take n        take n items off stack and print    take 4"
             else putStrLn $ "** Invalid command '" ++ str ++ "' **\n    try 'help' for full list of commands"
         getUserInput
+
+shouldShow :: String -> [String] -> Bool
+shouldShow f xs = f == "show" && xs == []
 
 shouldExit :: String -> [String] -> Bool
 shouldExit f xs = f == "exit" && xs == []
@@ -66,3 +74,6 @@ addToStack xs as = (reverse as) ++ xs
 popStack :: Stack -> Int -> (Stack, [String])
 popStack [] _ = ([], [])
 popStack xs i = (drop i xs, take i xs)
+
+showStack :: Stack -> [String]
+showStack xs = reverse xs
